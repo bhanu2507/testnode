c3e504961b4787b7528542c2aee6fd0aeecc80ec
Bhanu Mokkala <bhanureddy@gmail.com>
2015-09-25 16:07:37 +0530

    Working copy - Tabbed areas - listing of restaurants as per the area

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var blist;
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var db = req.db;
     var collection = db.get('restaurants');
     collection.distinct(("borough"),function(e,docs){
-        res.render('index', {
-            "restlist" : docs,
-              title: 'Express'
+       res.render('index', {
+            "blist" : docs,
+            title: 'testnode',
+           "restlist" : ''
         });
     });
-  //res.render('index', { title: 'Express' });
 });
 
 router.get('/:id', function(req, res, next) {
     var id = req.params.id;
     var db = req.db;
     var collection = db.get('restaurants');
-    collection.find({borough: id},{},function(e,docs){
-        res.render('list', {
+        collection.find({borough: id},{},function(e,docs){
+/**        res.append('index', {
             "restlist" : docs,
             title: id
+        }); **/
+        console.log(docs);
+        collection.distinct(("borough"),function(e,docs1){
+/**            res.render('index', {
+                "blist" : docs1,
+                title: 'testnode'
+            });**/
+        res.render('index', { "restlist" : docs, "blist" : docs1, title : id});
+            console.log(docs1);
         });
     });
     //res.render('index', { title: 'Express' });
 });
-
 
 module.exports = router;

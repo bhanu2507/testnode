fc2d3b0a693c7d7e070828cf095c8d291376a76d
Bhanu Mokkala <bhanureddy@gmail.com>
2015-09-29 11:07:51 +0530

    tabbed listing

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -43,8 +44,11 @@ app.use(function(req,res,next){
   next();
 });
 
+
 app.use('/', routes);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var totalrests = '';
+    pageSize = 100;
+    pageCount = '';
+    currentPage = 1;
+    rests = [];
+    restsArrays = [];
+    restsList = [];
+
 var blist;
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
     var db = req.db;
     var collection = db.get('restaurants');
@@ -15,26 +24,55 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.get('/:id', function(req, res, next) {
-    var id = req.params.id;
+
+
+router.param('locname', function(req, res, next, locname) {
+
+    // save name to the request
+    req.locname = locname;
+
+    next();
+});
+router.get('/:locname/', function(req, res, next) {
+    var id = req.locname;
     var db = req.db;
+
     var collection = db.get('restaurants');
         collection.find({borough: id},{},function(e,docs){
-/**        res.append('index', {
-            "restlist" : docs,
-            title: id
-        }); **/
-        console.log(docs);
+            totalrests = docs.slice();
+            pageCount = (docs.length)/pageSize;
+
+        //set current page if specifed as get variable (eg: /?page=2)
+        if (typeof req.query.page !== 'undefined') {
+            currentPage = +req.query.page;
+        }
+        else
+        {
+            restsArrays = [];
+            currentPage = 1;
+        }
+
+        //split list into groups
+        while (totalrests.length > 0) {
+            restsArrays.push(totalrests.splice(0, pageSize));
+        }
+
+        //show list of students from group
+        restsList = restsArrays[+currentPage - 1];
+
         collection.distinct(("borough"),function(e,docs1){
-/**            res.render('index', {
-                "blist" : docs1,
-                title: 'testnode'
-            });**/
-        res.render('index', { "restlist" : docs, "blist" : docs1, title : id});
-            console.log(docs1);
+        res.render('index', { "restlist" : docs,
+            "blist" : docs1,
+            title : id,
+            rests : restsList,
+            pageSize : pageSize,
+            totalrests : docs.length,
+            pageCount : Math.round(pageCount),
+            currentPage : currentPage
+        });
+        console.log("rests" + totalrests.length);
         });
     });
-    //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
